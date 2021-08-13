2f126ab6ddb5216de6dd3d8387bb07308414ab94
Golubova Viktoria <viki12007@inbox.ru>
2021-08-13 14:26:35 +0300

    lesson 3

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 
 <script>
 import vMainWrapper from './components/v-main-wrapper.vue'
+
 export default {
   name: 'App',
   components: {
--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,24 +1,39 @@
 <template>
 <div class="v-catalog-item">
-   <img src="" alt="img">
-   <p>Icon 1</p>
-   <p>Price: 100</p>
-   <button>Add to card</button>
+   <img class="v-catalog-item__image" :src=" require('../assets/images/' + product_data.image) " alt="img">
+   <p class="v-catalog-item__name">{{product_data.name}}</p>
+   <p class="v-catalog-item__price">Price: {{product_data.price}} p.</p>
+   <button 
+   class="v-catalog-item__btn btn" 
+   @click="sendDataToParent"
+   >Add to cart</button>
 </div>
 </template>
 
 <script>
+
 export default {
    name: 'v-catalog-item',
    components: {},
-   props: {},
+   props: {
+       product_data: {
+           type: Object,
+           default() {
+             return {}
+           }
+       }
+   },
    data() {
        return {
            title: 'v-catalog-item',
        }
    },
    computed: {},
-   metods: {},
+   methods: {
+       sendDataToParent() {
+           this.$emit('sendDataToParent', this.product_data.article)
+       }
+   },
    watch: {},
    mounted() {
        console.log('Hello world!')
@@ -29,9 +44,11 @@ export default {
 <style lang="scss">
 .v-catalog-item {
     flex-basis: 25%;
-    color: $red;
     box-shadow: 0 0 8px 0 #e0e0e0;
     padding: 16px;
     margin-bottom: 16px;
+    &__image {
+        width: 100px;
+    }
 } 
 </style>
